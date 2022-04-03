edaa7645aa2a1658d7045d600704d5cfae0e6db6
Christina Sok <christin11@outlook.com>
2022-04-03 14:02:48 -0400

    aggregating dataframes completed

--- datacamp/data_manipulation_with_pandas.py
+++ datacamp/data_manipulation_with_pandas.py
@@ -135,3 +135,60 @@ unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"
 # Print unemp_fuel_stats
 print(unemp_fuel_stats)
 
+# pivot tables
+"""
+dogs.pivot_table(values="weight_kg", index="color")
+# takes defulted .mean() of each group
+
+# different statistics (other than mean)
+# import numpy as np 
+dog.pivot_table(values="weight_kg", index="color", aggfunc=np.median)
+
+# multiple statistics 
+dogs.pivot_table(values="weight_kg", index="color", aggfunc=[np.mean, np.median])
+
+# pivot on two variables
+dog.pivot_table(values="weight_kg", index="color, columns="breed")
+
+# filling missing values in pivot tables 
+dogs.pivot_tables(values="weight_kg", index="color", colums="breed", fill_values=0)
+
+# summing with pivot tables using margins=True
+dogs.pivot_tables(values="weight_kg", index="color", colums="breed", fill_values=0, margins=True)
+
+"""
+
+# Pivoting on one variable
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_type(values="weekly_sales", index="type")
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+
+# Import Dependencies
+import numpy as np
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table()
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales", index="type", aggfunc=[np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
+# Pivot for mean weekly_sales by store type and holiday
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales", index="type", columns="is_holiday")
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
+
+# Fill in missing values and sum values with pivot tables
+# Import Dependencies
+import pandas as pd
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0))
+
+# sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
