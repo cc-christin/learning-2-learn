1d5f331c26d9e2297544f20642a67b2ac1458932
Christina Sok <christin11@outlook.com>
2022-04-03 12:38:49 -0400

    counting categorical variables

--- datacamp/data_manipulation_with_pandas.py
+++ datacamp/data_manipulation_with_pandas.py
@@ -2,6 +2,9 @@
 ## Aggregating DataFrames
 ### Counting 
 
+# Import dependencies 
+# import pandas as pd
+
 # vet_visits is the df
 # dropping duplicate names
 vet_visits.drop_duplicates(subset="name")
@@ -16,8 +19,10 @@ unique_dogs["breed"].value_counts(sort=True)
 # proportions obtained by using normalize argument 
 unique_dogs["breed"].values_counts(normalize=True)
 
-
 # Dropping duplicates
+# Import dependencies 
+# import pandas as pd 
+
 # sales is the df
 # Drop duplicates store/type combinations 
 # drop_duplicates()
@@ -34,4 +39,26 @@ holiday_dates = sales[sales["is_holiday"]==True].drop_duplicates("date")
 
 # Print date column of holiday_dates
 print(holiday_dates["date"])
+
+# Counting categorical variables 
+
+# Count the number of stores of each type
+# df -> store_types
+store_counts = store_types["type"].value_counts()
+print(store_counts)
+
+# Get the proportion of stores of each type
+# normalize argument
+store_props = store_types["type"].value_counts(normalize=True)
+print(store_props)
+
+# Count the number of each department number and sort
+# df -> store_depts
+# sort argument
+dept_counts_sorted = store_depts["department"].value_counts(sort=True)
+print(dept_counts_sorted)
+
+# Get the proportion of departments of each number and sort
+dept_props_sorted = store_depts["department"].value_counts(sort=True, normalizze=True)
+print(dept_props_sorted)
 
